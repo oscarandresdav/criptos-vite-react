0262b3fadd1337b798c6f5f0909b436dcea959a2
Oscar Davila <oscar7822002@gmail.com>
2022-09-23 08:24:27 -0500

    Creando el Input de Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import styled from "@emotion/styled"
-import ImagenCripto from './img/imagen-criptos.png';
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+import ImagenCripto from "./img/imagen-criptos.png";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -10,19 +11,18 @@ const Contenedor = styled.div`
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
   }
-`
-
+`;
 
 const Imagen = styled.img`
   max-width: 400px;
   width: 80%;
   margin: 100px auto 0 auto;
   display: block;
-`
+`;
 
 const Heading = styled.h1`
-  font-family: 'Lato', sans-serif;
-  color: #FFF;
+  font-family: "Lato", sans-serif;
+  color: #fff;
   text-align: center;
   font-weight: 700;
   margin-top: 80px;
@@ -30,28 +30,25 @@ const Heading = styled.h1`
   font-size: 34px;
 
   &::after {
-    content: '';
+    content: "";
     width: 100px;
     height: 6px;
-    background-color: #66A2FE;
+    background-color: #66a2fe;
     display: block;
     margin: 10px auto 0 auto;
   }
-`
+`;
 
 function App() {
-
   return (
     <Contenedor>
-      <Imagen 
-        src={ImagenCripto}
-        alt='Imagen Criptomonedas'
-      />
+      <Imagen src={ImagenCripto} alt="Imagen Criptomonedas" />
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario />
       </div>
     </Contenedor>
-  )
+  );
 }
 
-export default App
+export default App;
