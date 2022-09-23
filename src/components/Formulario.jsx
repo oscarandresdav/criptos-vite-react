4561ad91ea75f065692d37465204f51083551273
Oscar Davila <oscar7822002@gmail.com>
2022-09-23 08:29:52 -0500

    Primeros pasos creando un Custom Hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,28 @@
 import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
   border: none;
   width: 100%;
   padding: 10px;
-  color: #FFF;
+  color: #fff;
   font-weight: 700;
   text-transform: uppercase;
   font-size: 20px;
   border-radius: 5px;
-  transition: background-color .3s ease;
+  transition: background-color 0.3s ease;
   &:hover {
-    background-color: #7A7DFE;
+    background-color: #7a7dfe;
     cursor: pointer;
   }
-`
+`;
 
 const Formulario = () => {
+  const [SelectMonedas] = useSelectMonedas();
+
+  SelectMonedas();
+
   return (
     <form>
       <InputSubmit type="submit" value="Cotizar" />
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,9 @@
+const useSelectMonedas = () => {
+  const SelectMonedas = () => {
+    console.log("Desde Select Monedas");
+  };
+
+  return [SelectMonedas];
+};
+
+export default useSelectMonedas;
