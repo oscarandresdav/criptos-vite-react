892a2f58a03b5f8471883fdc77b3abf166e615ea
Oscar Davila <oscar7822002@gmail.com>
2022-09-26 15:10:16 -0500

    Formateando el Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,15 +1,61 @@
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 100px;
+`;
+
+const Detalles = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     resultado;
 
   return (
-    <div>
-      <p>El precio es de: <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{LASTUPDATE}</span></p>
-    </div>
+    <Contenedor>
+      <Imagen
+        src={`https://www.cryptocompare.com/${IMAGEURL}`}
+        alt="Imagen Cripto"
+      />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Detalles>
+          Precio más alto del día: <span>{HIGHDAY}</span>
+        </Detalles>
+        <Detalles>
+          Precio más bajo del día: <span>{LOWDAY}</span>
+        </Detalles>
+        <Detalles>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Detalles>
+        <Detalles>
+          Última actualización: <span>{LASTUPDATE}</span>
+        </Detalles>
+      </div>
+    </Contenedor>
   );
 };
 
